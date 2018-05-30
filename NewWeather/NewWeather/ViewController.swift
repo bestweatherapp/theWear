1013791124aea2f66aab850978b0ffee8bd1634f
Valentina <val0904@yandex.ru>
2018-05-30 20:31:21 +0300

    new

---
+++ NewWeather/NewWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewWeather
+//
+//  Created by Валентина on 30.05.2018.
+//  Copyright © 2018 Валентина. All rights reserved.
+// new project
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
