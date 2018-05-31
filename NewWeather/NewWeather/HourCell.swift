4d56a2390abe990304737109f14f520a9bad8298
Maxim Reshetov <maximreshetov@macbook-pro-maxim.local>
2018-05-31 11:20:18 +0300

    // Second try

---
+++ NewWeather/NewWeather/HourCell.swift
@@ -0,0 +1,22 @@
+//
+//  HourCell.swift
+//  NewWeather
+//
+//  Created by Maxim Reshetov on 31.05.2018.
+//  Copyright © 2018 Валентина. All rights reserved.
+//
+
+import UIKit
+
+
+class HourCell: UICollectionViewCell {
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+  
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
