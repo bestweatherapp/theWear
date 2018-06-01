131b63f3364954ed129721202b0f6c10c1aea571
Maxim Reshetov <maximreshetov@macbook-pro-maxim.local>
2018-06-01 10:38:51 +0300

    //

--- theWearApp/theWearApp/AppDelegate.swift
+++ theWearApp/theWearApp/AppDelegate.swift
@@ -70,7 +70,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
