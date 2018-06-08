0b9a7450170ef36ef773cd913d59d12d8052c7f8
Maxim Reshetov <maximreshetov@macbook-pro-maxim.local>
2018-06-08 12:14:20 +0300

    SettingsViewController was improved

--- theWearApp/theWearApp/SettingsViewController.swift
+++ theWearApp/theWearApp/SettingsViewController.swift
@@ -14,17 +14,42 @@ class SettingsViewController: UIViewController {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.layer.cornerRadius = 35
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor(white: 1, alpha: 0.9)
         view.isHidden = true
         return view
     }()
     
     private let closeButton: UIButton = {
         let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(UIImage(named: "close"), for: .normal)
         button.addTarget(self, action: #selector(CloseSettings), for: .touchUpInside)
         return button
+    }()
+    
+    private let topLine: UIView = {
+        let line = UIView()
+        line.backgroundColor = UIColor.dark
+        return line
+    }()
+    
+    private let notificationLine: UIView = {
+        let line = UIView()
+        line.backgroundColor = UIColor.dark
+        return line
+    }()
+    
+    private let genderLine: UIView = {
+        let line = UIView()
+        line.backgroundColor = UIColor.dark
+        return line
+    }()
+    private let genderForLineLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.attributedText = NSAttributedString(string: "Who are you?", attributes: [NSAttributedStringKey.font: UIFont.init(name: "SFProDisplay-Ultralight", size: 12)!, NSAttributedStringKey.foregroundColor:UIColor.dark])
+        return label
     }()
     
     private let notifyInMorning: UILabel = {
@@ -78,6 +103,67 @@ class SettingsViewController: UIViewController {
         return text
     }()
     
+    private let notificationForLineLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.attributedText = NSAttributedString(string: "Notifications", attributes: [NSAttributedStringKey.font: UIFont.init(name: "SFProDisplay-Ultralight", size: 12)!, NSAttributedStringKey.foregroundColor:UIColor.dark])
+        return label
+    }()
+    
+    private let manButton: UIButton = {
+        let manGender = UIButton()
+        manGender.setTitle("Man", for: .normal)
+        manGender.setTitleColor(UIColor(red: 80/255, green: 80/255, blue: 80/255, alpha: 80), for: .normal)
+        manGender.titleLabel?.font = UIFont(name: "SFProDisplay-Light", size: 16)
+        manGender.addTarget(self, action: #selector(manWasChoosen), for: .touchUpInside)
+        return manGender
+    }()
+    @objc func manWasChoosen() {
+        if UserDefaults.standard.string(forKey: "Gender") == "Man" { } else {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.manButton.titleLabel?.font = UIFont(name: "SFProDisplay-Medium", size: 17)
+                self.womanButton.titleLabel?.font = UIFont(name: "SFProDisplay-Light", size: 16)
+                self.view.layoutIfNeeded()
+            })
+            UserDefaults.standard.setValue("Man", forKey: "Gender")
+        }
+        if UserDefaults.standard.string(forKey: "Gender") == nil {
+            UserDefaults.standard.setValue("Man", forKey: "Gender")
+        }
+    }
+    private let womanButton: UIButton = {
+        let womanGender = UIButton()
+        womanGender.setTitle("Woman", for: .normal)
+        womanGender.setTitleColor(UIColor(red: 80/255, green: 80/255, blue: 80/255, alpha: 80), for: .normal)
+        womanGender.titleLabel?.font = UIFont(name: "SFProDisplay-Light", size: 16)
+        womanGender.addTarget(self, action: #selector(womanWasChoosen), for: .touchUpInside)
+        return womanGender
+    }()
+    @objc func womanWasChoosen() {
+        if UserDefaults.standard.string(forKey: "Gender") == "Woman" { } else {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.womanButton.titleLabel?.font = UIFont(name: "SFProDisplay-Medium", size: 17)
+                self.manButton.titleLabel?.font = UIFont(name: "SFProDisplay-Light", size: 16)
+                self.view.layoutIfNeeded()
+            })
+            UserDefaults.standard.setValue("Woman", forKey: "Gender")
+        }
+        if UserDefaults.standard.string(forKey: "Gender") == nil {
+            UserDefaults.standard.setValue("Woman", forKey: "Gender")
+        }
+    }
+    
+    private let temperatureForLineLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.attributedText = NSAttributedString(string: "Temperature", attributes: [NSAttributedStringKey.font: UIFont.init(name: "SFProDisplay-Ultralight", size: 12)!, NSAttributedStringKey.foregroundColor:UIColor.dark])
+        return label
+    }()
+    
     private let tempSegmentedControl: UISegmentedControl = {
         let sc = UISegmentedControl()
         sc.translatesAutoresizingMaskIntoConstraints = false
@@ -113,27 +199,46 @@ class SettingsViewController: UIViewController {
     }
     
     private func Layout() {
-        view.addSubview(closeButton)
-        view.addSubview(tempLabel)
-        view.addSubview(windLabel)
-        view.addSubview(pressureLabel)
-        view.addSubview(notificationsLabel)
-        view.addSubview(notifyInMorning)
-        view.addSubview(onMorning)
+        [closeButton, topLine, tempLabel, temperatureForLineLabel, windLabel, pressureLabel, notificationsLabel, notifyInMorning, onMorning, notificationLine, genderLine, notificationForLineLabel, genderForLineLabel, manButton, womanButton, datePickerView].forEach {view.addSubview($0)}
+        if UserDefaults.standard.string(forKey: "Gender") == "Man" {
+            self.manButton.titleLabel?.font = UIFont(name: "SFProDisplay-Medium", size: 17)
+        } else {
+            self.womanButton.titleLabel?.font = UIFont(name: "SFProDisplay-Medium", size: 17)
+        }
+        
+        var padding: CGFloat = 0
+        
+        switch UIScreen.main.nativeBounds.height {
+        case 1136:
+            print("iPhone 5")
+            padding = 22
+        case 1334:
+            print("iPhone 7")
+            padding = 33
+        case 1920:
+            print("iPhone 7+")
+            padding = 40.8
+        case 2436:
+            print("iPhone X")
+            padding = 33
+        default:
+            return
+        }
+        
         let tempChoose = ["°C", "°F"]
         let tempSegmentedControl = UISegmentedControl(items: tempChoose)
         tempSegmentedControl.selectedSegmentIndex = 0
         tempSegmentedControl.tintColor = .white
         tempSegmentedControl.layer.borderWidth = 1
-        tempSegmentedControl.layer.borderColor = UIColor(red: 107/255, green: 107/255, blue: 107/255, alpha: 100).cgColor
+        tempSegmentedControl.layer.borderColor = UIColor.dark.cgColor
         tempSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
         tempSegmentedControl.layer.cornerRadius = 15
-        tempSegmentedControl.backgroundColor = UIColor(red: 107/255, green: 107/255, blue: 107/255, alpha: 100)
+        tempSegmentedControl.backgroundColor = UIColor.dark
         tempSegmentedControl.addTarget(self, action: #selector(changeValue), for: .valueChanged)
         view.addSubview(tempSegmentedControl)
         
         tempSegmentedControl.centerYAnchor.constraint(equalTo: tempLabel.centerYAnchor).isActive = true
-        tempSegmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        tempSegmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding).isActive = true
         tempSegmentedControl.widthAnchor.constraint(equalToConstant: 90).isActive = true
         tempSegmentedControl.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
@@ -142,34 +247,29 @@ class SettingsViewController: UIViewController {
         windSegmentedControl.selectedSegmentIndex = 0
         windSegmentedControl.tintColor = .white
         windSegmentedControl.layer.borderWidth = 1
-        windSegmentedControl.layer.borderColor = UIColor(red: 107/255, green: 107/255, blue: 107/255, alpha: 100).cgColor
-        windSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        windSegmentedControl.layer.borderColor = UIColor.dark.cgColor
         windSegmentedControl.layer.cornerRadius = 15
-        windSegmentedControl.backgroundColor = UIColor(red: 107/255, green: 107/255, blue: 107/255, alpha: 100)
+        windSegmentedControl.backgroundColor = UIColor.dark
         windSegmentedControl.addTarget(self, action: #selector(changeValue), for: .valueChanged)
         view.addSubview(windSegmentedControl)
         
+        windSegmentedControl.anchor(top: nil, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: padding), size: .init(width: 90, height: 30))
         windSegmentedControl.centerYAnchor.constraint(equalTo: windLabel.centerYAnchor).isActive = true
-        windSegmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        windSegmentedControl.widthAnchor.constraint(equalToConstant: 90).isActive = true
-        windSegmentedControl.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
         let pressureChoose = ["x", "y"]
         let pressureSegmentedControl = UISegmentedControl(items: pressureChoose)
         pressureSegmentedControl.selectedSegmentIndex = 0
         pressureSegmentedControl.tintColor = .white
         pressureSegmentedControl.layer.borderWidth = 1
-        pressureSegmentedControl.layer.borderColor = UIColor(red: 107/255, green: 107/255, blue: 107/255, alpha: 100).cgColor
+        pressureSegmentedControl.layer.borderColor = UIColor.dark.cgColor
         pressureSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
         pressureSegmentedControl.layer.cornerRadius = 15
-        pressureSegmentedControl.backgroundColor = UIColor(red: 107/255, green: 107/255, blue: 107/255, alpha: 100)
+        pressureSegmentedControl.backgroundColor = UIColor.dark
         pressureSegmentedControl.addTarget(self, action: #selector(changeValue), for: .valueChanged)
         view.addSubview(pressureSegmentedControl)
         
+        pressureSegmentedControl.anchor(top: nil, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: padding), size: .init(width: 90, height: 30))
         pressureSegmentedControl.centerYAnchor.constraint(equalTo: pressureLabel.centerYAnchor).isActive = true
-        pressureSegmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        pressureSegmentedControl.widthAnchor.constraint(equalToConstant: 90).isActive = true
-        pressureSegmentedControl.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
         let notificationsSwitch = UISwitch()
         notificationsSwitch.setOn(false, animated: false)
@@ -181,41 +281,33 @@ class SettingsViewController: UIViewController {
         notificationsSwitch.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
         view.addSubview(notificationsSwitch)
         
+        notificationsSwitch.anchor(top: nil, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: padding), size: .init(width: 65, height: 20))
         notificationsSwitch.centerYAnchor.constraint(equalTo: notificationsLabel.centerYAnchor).isActive = true
         notificationsSwitch.centerXAnchor.constraint(equalTo: pressureSegmentedControl.centerXAnchor).isActive = true
-        notificationsSwitch.widthAnchor.constraint(equalToConstant: 65).isActive = true
-        notificationsSwitch.heightAnchor.constraint(equalToConstant: 20).isActive = true
         
-        view.addSubview(datePickerView)
-        datePickerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        datePickerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        datePickerView.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        datePickerView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        datePickerView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 250, left: 150, bottom: 250, right: 150), size: .init())
         
-
-        closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-        closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25).isActive = true
-        closeButton.widthAnchor.constraint(equalToConstant: 27).isActive = true
-        closeButton.heightAnchor.constraint(equalToConstant: 27).isActive = true
+        topLine.anchor(top: closeButton.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 25, left: padding, bottom: 0, right: padding), size: .init(width: 0, height: 0.5))
         
-        tempLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 140).isActive = true
-        tempLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        tempLabel.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        closeButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: 20), size: .init(width: 27, height: 27))
         
-        windLabel.topAnchor.constraint(equalTo: tempLabel.bottomAnchor, constant: 20).isActive = true
-        windLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        windLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        tempLabel.anchor(top: topLine.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 25, left: padding, bottom: 0, right: 0), size: .init(width: 120, height: 0))
+        temperatureForLineLabel.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        temperatureForLineLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        temperatureForLineLabel.centerYAnchor.constraint(equalTo: topLine.centerYAnchor).isActive = true
         
-        pressureLabel.topAnchor.constraint(equalTo: windLabel.bottomAnchor, constant: 20).isActive = true
-        pressureLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        pressureLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        windLabel.anchor(top: tempLabel.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 20, left: padding, bottom: 0, right: 0), size: .init(width: 100, height: 0))
         
-        notificationsLabel.topAnchor.constraint(equalTo: pressureLabel.bottomAnchor, constant: 60).isActive = true
-        notificationsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        notificationsLabel.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        pressureLabel.anchor(top: windLabel.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 20, left: padding, bottom: 0, right: 0), size: .init(width: 100, height: 0))
         
-        notifyInMorning.topAnchor.constraint(equalTo: notificationsLabel.bottomAnchor, constant: 40).isActive = true
-        notifyInMorning.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        notificationLine.anchor(top: pressureLabel.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 35, left: padding, bottom: 0, right: padding), size: .init(width: 0, height: 0.5))
+        notificationForLineLabel.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        notificationForLineLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        notificationForLineLabel.centerYAnchor.constraint(equalTo: notificationLine.centerYAnchor).isActive = true
+        
+        notificationsLabel.anchor(top: notificationLine.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 25, left: padding, bottom: 0, right: 0), size: .init(width: 150, height: 0))
+        
+        notifyInMorning.anchor(top: notificationsLabel.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 20, left: padding, bottom: 0, right: 0), size: .init())
         
         onMorning.centerYAnchor.constraint(equalTo: notifyInMorning.centerYAnchor).isActive = true
         onMorning.heightAnchor.constraint(equalToConstant: 20).isActive = true
@@ -223,6 +315,13 @@ class SettingsViewController: UIViewController {
         onMorning.widthAnchor.constraint(equalToConstant: 70).isActive = true
         onMorning.heightAnchor.constraint(equalToConstant: 20).isActive = true
         
+        genderLine.anchor(top: notifyInMorning.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 35, left: padding, bottom: 0, right: padding), size: .init(width: 0, height: 0.5))
+        genderForLineLabel.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        genderForLineLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        genderForLineLabel.centerYAnchor.constraint(equalTo: genderLine.centerYAnchor).isActive = true
+        
+        manButton.anchor(top: genderForLineLabel.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 20, left: (padding * 2.5), bottom: 0, right: 0), size: .init())
+        womanButton.anchor(top: genderForLineLabel.bottomAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 320, left: 0, bottom: 0, right: (padding * 2.5)), size: .init())
         
     }
 }
