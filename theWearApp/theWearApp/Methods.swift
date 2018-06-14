b7dccc20e5cc8d23abcfed158098cd6f4f4705ab
Valentina <val0904@yandex.ru>
2018-06-14 23:49:14 +0300

    //

--- theWearApp/theWearApp/Methods.swift
+++ theWearApp/theWearApp/Methods.swift
@@ -18,54 +18,54 @@ class Methods
             case 0..<4.5:
                 comment_ += "Feels slightly cooler than it seems. "
             case 4.5..<10:
-                comment_ += " Feels cooler than it seems."
+                comment_ += "Feels cooler than it seems. "
             default:
-                comment_ += " Feels considerably colder than it seems."
+                comment_ += "Feels considerably colder than it seems. "
             }
         default:
             comment_ += ""
         }
         switch (Int(Current.feelslike!)){
         case -50 ..< -30:
-            comment_ += " Extremely cold. Avoid being outside unless dressed properly! "
+            comment_ += "Extremely cold. Avoid being outside unless dressed properly! "
         case -30 ..< -10:
-            comment_ += " Very cold. Dress warmly! "
+            comment_ += "Very cold. Dress warmly! "
         case -10 ..< -5:
-            comment_ +=  " Frosty weather, put on your coat, scarf and gloves. "
+            comment_ +=  "Frosty weather, put on your coat, scarf and gloves. "
         case -5 ..< -3:
-            comment_ +=  " Frosty weather, put on your coat. "
+            comment_ +=  "Frosty weather, put on your coat. "
         case 0..<3:
-            comment_ += " Mind the freezing! Stay warm, put on a coat. "
+            comment_ += "Mind the freezing! Stay warm, put on a coat. "
         case 3..<7:
-            comment_ += " Quite cool, put on a coat. "
+            comment_ += "Quite cool, put on a coat. "
         case 7..<13:
-            comment_ += " Comfortable weather, put on a jacket. "
+            comment_ += "Comfortable weather, put on a jacket. "
         case 13..<18:
-            comment_ += " Quite warm, put on a hoodie and jeans. "
+            comment_ += "Quite warm, put on a hoodie and jeans. "
         case 18..<20:
-            comment_ += " Warm weather, put on a longsleeve and jeans. "
+            comment_ += "Warm weather, put on a longsleeve and jeans. "
         case 20..<23:
-            comment_ += " Comfortable warm weather, put on shirt. "
+            comment_ += "Comfortable warm weather, put on shirt. "
         case 23..<25:
-            comment_ += " Comfortable warm weather, put on a t-shirt and jeans. "
+            comment_ += "Comfortable warm weather, put on a t-shirt and jeans. "
         case  25..<35:
-            comment_ += " Hot weather, better put on a t-shirt and shorts  "
+            comment_ += "Hot weather, better put on a t-shirt and shorts  "
         case 30..<50:
-            comment_ += " Extremely hot! Put on the lightest clothes. "
+            comment_ += "Extremely hot! Put on the lightest clothes. "
         default:
-            comment_ = " There is no comment " }
+            comment_ = "There is no comment " }
         switch (Current.wind_speed!)
         { case 7...9:
-            comment_ += " Mind strong wind! "
+            comment_ += "Mind strong wind! "
         case 9...20:
-            comment_ += " Mind very strong wind! "
+            comment_ += "Mind very strong wind! "
         case 20...50:
-            comment_ += " Extremely strong wind! "
+            comment_ += "Extremely strong wind! "
             default: comment_ += ""  }
         if (day.uv! > 6.5 && day.uv! < 8)
-        {comment_ += " High UV. " }
+        {comment_ += "High UV. " }
         if (day.uv! > 8  )
-        { comment_ += " Very high UV! " }
+        { comment_ += "Very high UV! " }
         return comment_
     }
     
@@ -105,7 +105,7 @@ class Methods
         switch correctTemp
         {
         case -70 ..< -30 :
-            comment += " Extremely cold! Avoid being outside unless dressed up properly! "
+            comment += "Extremely cold! Avoid being outside unless dressed up properly! "
             if (gender == "Man"){
             images.append("coat_winter")
             images.append("jeans")
@@ -120,7 +120,7 @@ class Methods
                 images.append("gloves")
                 images.append("scarf")}
         case -30 ..< -10:
-            comment += " Very cold weather. Put on all the warmes clothes and don't say outside for too much. "
+            comment += "Very cold weather. Put on all the warmes clothes and don't say outside for too much. "
             if (gender == "Man"){
             images.append("coat_winter")
             images.append("jeans")
@@ -135,7 +135,7 @@ class Methods
                 images.append("gloves")
                 images.append("scarf")}
         case -10 ..< -5:
-            comment +=  " Cold frosty weather. Put on a winter coat, scarf and gloves. "
+            comment +=  "Cold frosty weather. Put on a winter coat, scarf and gloves. "
             if (gender == "Man"){
             images.append("coat-3")
             images.append("jeans")
@@ -150,7 +150,7 @@ class Methods
                 images.append("gloves")
                 images.append("scarf")}
         case -5 ..< -3:
-            comment +=  " Feels cold and freezing. Put on a coat and a cap. "
+            comment +=  "Feels cold and freezing. Put on a coat and a cap. "
             if (gender == "Man"){
             images.append("coat-3")
             images.append("jeans")
@@ -161,7 +161,7 @@ class Methods
                 images.append("women-boot")
                 images.append("cap")}
         case -3 ..< 0:
-            comment += " Freezing weather. Dress warmly, put on a coat and probably a cap. "
+            comment += "Freezing weather. Dress warmly, put on a coat and probably a cap. "
             if (gender == "Man"){
             images.append("coat-3")
             images.append("jeans")
@@ -169,7 +169,7 @@ class Methods
             images.append("cap")}
             else{}
         case 0..<3 where Int(day.avghumidity!)>70:
-            comment += " Freezing and humid weather. Put on a coat, gloves and a scarf.  "
+            comment += "Freezing and humid weather. Put on a coat, gloves and a scarf.  "
             if (gender == "Man"){
             images.append("coat-3")
             images.append("jeans")
@@ -181,7 +181,7 @@ class Methods
                 images.append("women-boot")
                 images.append("cap")}
         case 0..<3 :
-            comment += " Freezing and humid weather. Put on a coat and jeans. "
+            comment += "Freezing and humid weather. Put on a coat and jeans. "
             if (gender == "Man"){
             images.append("coat-3")
             images.append("jeans")
@@ -190,7 +190,7 @@ class Methods
                 images.append("slim-fit-pants")
                 images.append("women-boot")}
         case 3..<7 where Int(day.avghumidity!)>70:
-            comment += " Feels cool and humid. Put on a coat and probably a scarf. "
+            comment += "Feels cool and humid. Put on a coat and probably a scarf. "
             if (gender == "Man"){
             images.append("jacket")
             images.append("jeans")
@@ -199,7 +199,7 @@ class Methods
                 images.append("slim-fit-pants")
                 images.append("women-boot")}
         case 3..<7:
-            comment += " Feels cool, put on a jacket and jeans. "
+            comment += "Feels cool, put on a jacket and jeans. "
             if (gender == "Man"){
             images.append("jacket_")
             images.append("jeans")
@@ -208,7 +208,7 @@ class Methods
                 images.append("slim-fit-pants")
                 images.append("women-boot")}
         case 7..<13:
-            comment += " Comfortable cool weather. Put on a jacket and jeans. "
+            comment += "Comfortable cool weather. Put on a jacket and jeans. "
             if (gender == "Man"){
             images.append("jacket")
             images.append("jeans")
@@ -217,7 +217,7 @@ class Methods
                 images.append("slim-fit-pants")
                 images.append("women-boot")}
         case 13..<18:
-            comment += " Feels warm, probably put on a hoodie and jeans. "
+            comment += "Feels warm, probably put on a hoodie and jeans. "
             if (gender == "Man"){
             images.append("hoody")
             images.append("jeans")
@@ -226,7 +226,7 @@ class Methods
                 images.append("slim-fit-pants")
                 images.append("oxford-hell")}
         case 18..<20:
-            comment += " Feels warm, probably put on a longsleeve and jeans. "
+            comment += "Feels warm, probably put on a longsleeve and jeans. "
             if (gender == "Man"){
             images.append("longsleeve")
             images.append("jeans")
@@ -235,7 +235,7 @@ class Methods
                 images.append("skirt-3")
                 images.append("sneakers")}
         case 20..<23:
-            comment += " Feels warm, probably put on a shirt and jeans. "
+            comment += "Feels warm, probably put on a shirt and jeans. "
             if (gender == "Man"){
             images.append("shirt")
             images.append("jeans")
@@ -244,7 +244,7 @@ class Methods
                 images.append("skirt-3")
                 images.append("ballets-flats")}
         case 23..<25:
-            comment += " Comfortable warm weather. Put on a T-shirt and jeans. "
+            comment += "Comfortable warm weather. Put on a T-shirt and jeans. "
             if (gender == "Man"){
             images.append("polo")
             images.append("jeans")
@@ -255,7 +255,7 @@ class Methods
         case  25..<35:
             if (correctTemp > 29) && (Int(day.avghumidity!) > 70)
             {
-                comment += " Very hot outside. Mind the dehydration! Put on a t-shirt and shorts. "
+                comment += "Very hot outside. Mind the dehydration! Put on a t-shirt and shorts. "
                 if (gender == "Man"){
                 images.append("polo")
                 images.append("chino-shorts")
@@ -264,7 +264,7 @@ class Methods
                     images.append("skirt-2")
                     images.append("flat-shoes")}
             }
-            else { comment += " Very hot outside. Mind the sunstroke, please! "
+            else { comment += "Very hot outside. Mind the sunstroke, please! "
                 if (gender == "Man"){
                 images.append("polo")
                 images.append("chino-shorts")
@@ -276,7 +276,7 @@ class Methods
         case 35..<43:
             if (Int(day.avghumidity!)>30)
             {
-                comment += " Enormously hot, might be unbearable. Avoid being outside for too long! Don't wear dark colors.  "
+                comment += "Enormously hot, might be unbearable. Avoid being outside for too long! Don't wear dark colors. "
                 if (gender == "Man"){
                 images.append("tshirt")
                 images.append("chino-shorts")}
@@ -285,7 +285,7 @@ class Methods
                     images.append("flip-flops")}
 //                images.append("flops")
             }
-            else { comment += " Extremely hot. Be careful and avoid the sunlight. Don't wear dark colors."
+            else { comment += "Extremely hot. Be careful and avoid the sunlight. Don't wear dark colors. "
                 if (gender == "Man"){
                 images.append("tshirt")
                 images.append("chino-shorts")
@@ -295,7 +295,7 @@ class Methods
                     images.append("flip-flops")}
             }
         case 43..<100:
-            comment += " Enormously hot. Mind the risk of a sunstroke. Avoid being outside! Put on the lighest clothes. "
+            comment += "Enormously hot. Mind the risk of a sunstroke. Avoid being outside! Put on the lighest clothes. "
             if (gender == "Man"){
             images.append("tshirt")
             images.append("chino-shorts")
@@ -384,43 +384,43 @@ class Methods
             if (dayflag&&morningflag&&eveningflag&&nightflag)||(nightflag&&morningflag&&dayflag)||(morningflag&&dayflag&&eveningflag)||(dayflag&&eveningflag&&nightflag)||(morningflag&&eveningflag&&nightflag)
             {comment += "Rains during all the day possible. "}
             else if (dayflag&&morningflag)||(dayflag&&eveningflag)||(eveningflag&&morningflag)
-            { comment += " Mind possible rain during the day. "}
+            { comment += "Mind possible rain during the day. "}
             else if (morningflag)
-            {comment += " Mind rain in the morning. "}
+            {comment += "Mind rain in the morning. "}
             else if (dayflag)
-            {comment += " Rain possible. "}
+            {comment += "Rain possible. "}
             else if (eveningflag)
-            {comment += " Mind rains in the evening. "}
+            {comment += "Mind rains in the evening. "}
             else if (nightflag)
-            {comment += " Thunders possible. "}
+            {comment += "Thunders possible. "}
         }
         if (avgday - avgmorning > 6 )
         {
             if (avgday < 20)
-            {comment += " Significantly warmer in the afternoon. "}
+            {comment += "Significantly warmer in the afternoon. "}
             else
-            {comment += " Significantly hotter in the afternoon. "}
+            {comment += "Significantly hotter in the afternoon. "}
         }
         if (avgevening - avgday > 6) && (avgday > 5)
         {
-            comment += " Evening will be cooler "
+            comment += "Evening will be cooler. "
         }
         if (Int(day.uv!) > 8 )//проверка!!! Анадырь 3999
         {
-            comment += " Mind high UV index. Don't forget about sun-protecting products "
+            comment += "Mind high UV index. Don't forget about sun-protecting products. "
             images.append("glasses")
         }
         if (day.temperature_max! - day.temperature_min! > 12)
         {
-            comment += " Considerable temperature difference possible. "
+            comment += "Considerable temperature difference possible. "
         }
         if (day.windSpeed_max! > 10 && day.windSpeed_max! < 20 )
         {
-            comment += " Mind strong wind! "
+            comment += "Mind strong wind! "
         }
         if (day.windSpeed_max! > 20)
         {
-            comment += " Mind very strong wind! "
+            comment += "Mind very strong wind! "
         }
         return (comment, images)
     }
@@ -462,7 +462,7 @@ class Methods
         let high = bounds.1
         var sum = 0.0
         for i in low...high
-        {
+        {//
             if (rainFlag == false && (allhours[i].will_it_rain == 1||Int(allhours[i].chance_of_rain!)!>49||allhours[i].condition!.range(of: "rain") != nil)||(allhours[i].condition!.range(of: "rains") != nil)||(allhours[i].condition!.range(of: "sleet") != nil)||(allhours[i].condition!.range(of: "drizzle") != nil)||(allhours[i].condition!.range(of: "shower") != nil)||(allhours[i].condition! == "Heavy rain")||(allhours[i].condition!.range(of: "showers") != nil ))
             { rainFlag = true     }
             sum += allhours[i].temperature!
